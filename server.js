df710457a7126b3fc002cde6985896cc78b008b0
herbiemolina <herbiemolina@gmail.com>
2019-04-17 13:58:40 -0400

    server

---
+++ server.js
@@ -0,0 +1,26 @@
+// Dependencies
+var express = require("express");
+var path = require("path");
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Create express app instance.
+var app = express();
+
+//data parsing handled by Express
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//routes
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+//Start the server
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
